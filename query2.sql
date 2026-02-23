de176ddac9311ce710dfb5393703679a1c867887
ghifar1327 <givartorreto@gmail.com>
2026-02-23 21:41:30 +0700

    feat: adds query 2 'query join'

--- query2.sql
+++ query2.sql
@@ -1,31 +1,25 @@
--- no.1
-SELECT movies.name, movies.year, movies.year , movies.rankscore ,directors.first_name, movies_genres.genre
+SELECT 
+    movies.id AS "id",
+    movies.name AS "Title Movie",
+    movies_genres.genre AS "Genre",
+    directors.first_name AS "Director"
 FROM movies
 JOIN movies_directors
-  ON movies.id = movies_directors.movie_id
-JOIN directors
-  ON directors.id = movies_directors.director_id
+    ON movies.id = movies_directors.movie_id
 JOIN movies_genres
-  ON movies.id = movies_genres.movie_id
+    ON movies_genres.movie_id = movies.id
+JOIN directors
+    ON directors.id = movies_directors.director_id
 LIMIT 50;
 
 
-SELECT actors.first_name as "actors", movies.name as "movies_title", roles."role" FROM actors
+
+SELECT 
+  actors.id AS "id", 
+  actors.first_name AS "Actor", 
+  movies.name AS "Title Movie"
+FROM actors 
 JOIN roles 
-    ON actors.id = roles.actor_id
+  ON roles.actor_id = actors.id 
 JOIN movies 
-    ON roles.movie_id = movies.id
-LIMIT 50
-
-
-SELECT * FROM roles LIMIT 1
--- SELECT * From movies_directors limit 50;
-
--- SELECT * FROM movies_genres limit 1
-
--- SELECT "actors"."first_name" FROM actors
-
--- SELECT * FROM actors limit 1
-
-
--- SELECT * FROM role limit 1
+  ON roles.movie_id = movies.id;
--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
+# query 1
 ![img1](./img1.png)
+
+# query 2
+![query_2.1](./img2.1.png)
+![quer_2.2](./img.2.2.png)
